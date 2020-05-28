c64e77d390d4cd65e9d8629ae325ba29eae955b0
Prasoon Mishra <63459571+darthlyvading@users.noreply.github.com>
2020-05-28 14:06:33 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+terms = int(input("How many terms? "))
+n1 = 0
+n2 = 1
+count = 0
+if terms <= 0:
+   print("terms should be > 0")
+elif terms == 1:
+   print("Fibonacci series of ",terms," is :")
+   print(n1)
+else:
+   print("Fibonacci series is :")
+   while count < terms:
+       print(n1)
+       total = n1 + n2
+       n1 = n2
+       n2 = total
+       count += 1
